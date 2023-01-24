5900d964aabae10313f64c4a10da82f0227647aa
igor <igorkhodakivskyi888@gmail.com>
2023-01-24 14:20:28 +0900

    main-push

---
+++ external-assets/external_config.js
@@ -0,0 +1,31 @@
+// =========================================================
+// get config from URL parameters
+// =========================================================
+
+function getQueryParams(qs) {
+   qs = qs.split('+').join(' ');
+   var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
+   while (tokens = re.exec(qs)) params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+   return params;
+}
+
+var query = {}
+try { query = getQueryParams(document.location.search); } catch (e) { }
+// console.log(query.gameName);
+
+
+// =========================================================
+// Hard coded configs
+// =========================================================
+
+window._EXTERNAL_CONFIGS = {
+   externalServerIP: 'http://192.168.114.59:13000',    // comment out this line to switch to default server at baohule.com
+
+   isEmbeddedToWeb: false,
+   autoLoadGameSceneName: query.gameName,      // "game_baijialeQZ" or "game_bairenniuniu"
+   autoLoginCredential: {
+      username: 'robot1',
+      userid: 1,
+      sign: 'ASKFO312r2fno22d',
+   }
+};
